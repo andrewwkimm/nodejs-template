c4570f092f7bd63c3cd17487c94eab95761728bd
andrewwkimm <andrewkimka@gmail.com>
2026-03-10 22:12:49 -0400

    Remove .tsx related configs

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,11 @@
 import js from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
-import eslintReact from "@eslint-react/eslint-plugin";
 
 export default [
   js.configs.recommended,
   {
-    files: ["src/**/*.{ts,tsx}"],
+    files: ["src/**/*.ts"],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
@@ -16,11 +15,9 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tseslint,
-      ...eslintReact.configs.recommended.plugins,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      ...eslintReact.configs.recommended.rules,
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/no-explicit-any": "error",
